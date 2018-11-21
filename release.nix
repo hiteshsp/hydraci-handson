d4228b6c3e50db087348d6473a1a0fd63ce12a69
Hitesh SP <hiteshsp@outlook.com>
2018-11-21 17:33:22 +0530

    hello

--- release.nix
+++ release.nix
@@ -1,23 +1,19 @@
-let
-  pkgs = import <nixpkgs> {}; 
+{ supportedSystems ? ["i686-linux" "x86_64-linux"] }:
 
-  jobs = rec { 
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
 
-    tarball = 
-      pkgs.releaseTools.sourceTarball { 
-        name = "hello-tarball";
-        src = <hello>; 
-        buildInputs = (with pkgs; [ gettext texLive texinfo ]);
-      };
-    build = 
-      { system ? builtins.currentSystem }:  
+{
 
-      let pkgs = import <nixpkgs> { inherit system; }; in
-      pkgs.releaseTools.nixBuild { 
-        name = "hello";
-        src = jobs.tarball;
-        configureFlags = [ "--disable-silent-rules" ];
-      };
-  };
-in
-  jobs 
+  # Simply assign a derivation to an attribute to have it built.
+  hello_world_1 = pkgs_x86_64_linux.hello;
+
+  # 'hydraJob' strips all non-essential attributes.
+  hello_world_2 = pkgs.lib.hydraJob pkgs_x86_64_linux.hello;
+
+  # Generate one attribute per supported platform.
+  hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
+
+} // mapTestOn {
+
+  # Fancy shortcut to generate one attribute per supported platform.
+  hello = supportedSystems;
